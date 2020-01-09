f26c6106dec141e79b05809fb8a175119c562f81
Aleš Rybák <ales.rybak@lundegaard.eu>
2020-01-09 15:10:27 +0100

    feature: Added default phase

--- src/main/java/eu/lundegaard/maven/finddeps/FindDepsMojo.java
+++ src/main/java/eu/lundegaard/maven/finddeps/FindDepsMojo.java
@@ -26,6 +26,7 @@ import org.apache.maven.model.Repository;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
@@ -50,6 +51,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 @Mojo(
         name = "find-deps",
+        defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
         requiresDependencyResolution = ResolutionScope.TEST)
 public class FindDepsMojo extends AbstractMojo {
 
